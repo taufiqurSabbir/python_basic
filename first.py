418e59b8a8174aa974c14e2be9a55b8cb22edf7c
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-01-14 23:21:08 +0600

    python basic

---
+++ first.py
@@ -0,0 +1,80 @@
+print('Taufiqur Sabbir')
+batch=input('Enter Your batch number:')
+
+#valid variable one
+astra='variable one'
+print(astra)
+
+#valid variable two
+
+Name="variable two"
+print(Name)
+
+#valid variable three
+
+my_skill='php,laravel,javascript,python'
+print(my_skill)
+
+#valid variable four
+
+StillLearning = 'Python,django'
+print(StillLearning)
+
+#valid variable five
+
+variableFive='five variable'
+print(variableFive)
+
+
+#Pascal Case Variable
+
+TestVariableSix='pascal case variable'
+
+
+#single value to Multiple variables
+
+laravel=django=express=asp ='Web Backend Tech'
+
+#Print
+print(laravel)
+print(django)
+print(express)
+print(asp)
+
+#memory location
+print(id(laravel))
+print(id(django))
+print(id(express))
+print(id(asp))
+
+#input float
+floatInput = float(input('Enter float value:'))
+print(floatInput)
+#a = ‘Welcome to Django for web and Ai’ convert into upper case.
+
+
+a = 'Welcome to Django for web and Ai'
+print(a.upper())
+
+#b = ‘ Python’ remove whitespace.
+
+b = '  Python'
+print(b.strip())
+
+#int to flot
+c = 60
+d= 22
+
+print(float(c))
+print(float(d))
+
+#dictionary
+
+userInfo={
+    'name':'taufiqur',
+    'age':24,
+    'skill':'Web Developer',
+
+}
+
+print(userInfo)
