8517d88c68ac745287fb4d7482899398dbc13684
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-01-16 00:03:18 +0600

    List CURD

---
+++ second.py
@@ -0,0 +1,43 @@
+language_list= ['python','Javascript','java','php','go']
+
+
+while True:
+    task = input('Input what do you want? read/create/update/delete: ')
+#read list
+    if task=='read':
+        print('language',language_list)
+
+#Insert new item to list
+    elif task =='create':
+        pos=int(input('Enter position of insert:'))
+        if pos <= len(language_list)+1:
+            value = input('Enter The value:')
+            language_list.insert((pos-1),value)
+            print('language', language_list)
+        else:
+            print('Trying to insert in wrong position')
+ #Update item
+    elif task=='update':
+        pos = int(input('Enter position of Update:'))
+        if  pos <= len(language_list):
+            value = input('Enter The value:')
+            language_list[pos-1]=value
+            print('language', language_list)
+        else:
+            print('Trying to update in wrong position')
+#delete item
+    elif task=='delete':
+        con =input('delete with position or value?')
+        if con=='position':
+            pos = int(input('Enter position of Delete:'))
+            if pos <= len(language_list):
+                language_list.pop(pos-1)
+                print('language', language_list)
+            else:
+                print('Wrong Position')
+        elif(con=='value'):
+            pos = (input('Enter value of Delete:'))
+            language_list.remove(pos)
+            print('language', language_list)
+        else:
+            print('Wrong position or value not in list')
